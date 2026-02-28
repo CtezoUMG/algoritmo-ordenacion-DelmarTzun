5848dbaf494074950c49a0308a49018a3ea839d1
Delmar Danilo Tzun Perez <dtzunp@miumg.edu.gt>
2026-02-28 08:59:03 -0600

    Update Algoritmo.cs

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -52,4 +52,43 @@ public class Algoritmo
 
 
     }
+
+
+    public void QuickSort(int[] arr)
+{
+    QuickSort(arr, 0, arr.Length - 1);
+}
+
+private void QuickSort(int[] arr, int left, int right)
+{
+    if (left < right)
+    {
+        int pivotIndex = Partition(arr, left, right);
+        QuickSort(arr, left, pivotIndex - 1);
+        QuickSort(arr, pivotIndex + 1, right);
+    }
+}
+
+private int Partition(int[] arr, int left, int right)
+{
+    int pivot = arr[right]; // elegir el último como pivote
+    int i = left - 1;
+    for (int j = left; j < right; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            Swap(ref arr[i], ref arr[j]);
+        }
+    }
+    Swap(ref arr[i + 1], ref arr[right]);
+    return i + 1;
+}
+
+private void Swap(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
 }
